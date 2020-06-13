2afbd689a6f5a9c1e9e6a7aab634e9ca350b93b8
siragasnow <siraga613@gmail.com>
2020-06-13 16:37:00 +0900

    [add]5-9 users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   root 'home#top'
   get 'home/about' => 'home#about', as: 'about'
   devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
