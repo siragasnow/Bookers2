12686235e3b7e80ac49d8177f8e4761f526db88e
siragasnow <siraga613@gmail.com>
2020-06-13 14:55:50 +0900

    [add/update]5-9 top/about view,books controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root 'homes#top'
-  get 'homes/about'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  root 'home#top'
+  get 'home/about' => 'home#about', as: 'about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
