c74f484ced239aeccd6053475326536a1855cded
siragasnow <siraga613@gmail.com>
2020-06-14 18:06:42 +0900

    [update]5-9 design

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,47 @@
 class BooksController < ApplicationController
   def index
+  	@book = Book.new
+  	@books = Book.all
+  	@user = User.find(current_user.id)
+  	@users =User.all
   end
 
   def show
+  	@book = Book.find(params[:id])
+  	@user = User.find(@book.user_id)
+  	@book_new = Book.new
+  end
+
+  def new
+  	@book = Book.new
+  end
+
+  def create
+  	@user = User.find(current_user.id)
+  	@book = Book.new(book_params)
+  	@book.user_id = current_user.id
+  	@book.save
+  	redirect_to books_path
   end
 
   def edit
+  	@user = current_user
+  	@user = User.find(params[:id])
   end
+
+  def update
+  	@book = Book.find(params[:id])
+  	@book.update(book_params)
+  	redirect_to book_path(@book.id)
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
+  def user_params
+  	params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   	@users = User.all
+    @user = current_user
   end
 
   def show
   	@user = User.find(params[:id])
+  	@books = @user.books.page(params[:page]).reverse_order
   end
 
   def edit
   	@user = User.find(params[:id])
   end
+
+  def update
+  	@user = User.find(params[:id])
+  	@user.update(user_params)
+  	redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+  params.requir(:user).permit(:name, :introduction :profile_image)
+  end
+
 end
